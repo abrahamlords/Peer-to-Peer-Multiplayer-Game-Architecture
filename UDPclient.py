091f3da1d5d42ebe0ffb85b23ed0161786c5bbf1
abrahamlords <159593762+abrahamlords@users.noreply.github.com>
2024-04-24 00:55:25 -0700

    Add files via upload
    part 2

--- UDPclient.py
+++ UDPclient.py
@@ -1,26 +1,10 @@
 from socket import *
-import sys
-serverName = sys.argv[1]
-serverPort = 51000
+serverName = '128.138.32.126'
+serverPort = 12000
 
-clientSocket1 = socket(AF_INET, SOCK_DGRAM)
-clientSocket1.bind(('10.120.70.145', 51001))
-clientSocket2 = socket(AF_INET, SOCK_DGRAM)
-clientSocket2.bind(('10.120.70.145', 51002))
-
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
-
-clientSocket1.sendto(message.encode(), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket1.recvfrom(2048)  # buffer size = 2048
-massage, serverAddress = clientSocket1.recvfrom(2048)  # buffer size = 2048
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
-print(massage.decode())
-
-try:
-	plzno, serverAddress = clientSocket2.recvfrom(2048, MSG_DONTWAIT)  # buffer size = 2048
-	print(plzno.decode())
-except:
-	print('no massange')
-
-
-clientSocket1.close()
+clientSocket.close()
--- UDPserver.py
+++ UDPserver.py
@@ -1,15 +1,9 @@
 from socket import *
-import sys
-serverPort = 51000  #51000 to 51499
-
-
+serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)  # buffer size = 2048
-
+    message, clientAddress = serverSocket.recvfrom(2048)
     modifiedMessage = message.decode().upper()
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-    serverSocket.sendto(message, ('10.120.70.145', 51001))
-    serverSocket.sendto(message, ('10.120.70.145', 51002))
